853a6543f6f450c32d78352eeaa760675928192c
George-Kovalenko <kovalenkogosha@gmail.com>
2017-03-15 22:13:18 +0300

    Added Controller

---
+++ ContactListWeb/src/main/java/edu/itechart/contactlist/controller/Controller.java
@@ -0,0 +1,31 @@
+package edu.itechart.contactlist.controller;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Controller extends HttpServlet {
+    private static final Logger LOGGER = LogManager.getLogger(Controller.class);
+
+    public Controller() {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}

e69cc1b9a18ad3eaeacc8a8e4e04689cc412962e
George-Kovalenko <kovalenkogosha@gmail.com>
2017-04-09 21:13:05 +0300

    Fixed gender field handler

--- contact-list-web/src/main/java/edu/itechart/contactlist/handler/fieldhandlers/GenderFieldHandler.java
+++ contact-list-web/src/main/java/edu/itechart/contactlist/handler/fieldhandlers/GenderFieldHandler.java
@@ -3,14 +3,15 @@ package edu.itechart.contactlist.handler.fieldhandlers;
 import edu.itechart.contactlist.entity.Contact;
 import edu.itechart.contactlist.handler.FieldHandlerException;
 import edu.itechart.contactlist.util.Validator;
+import org.apache.commons.lang3.StringUtils;
 
 public class GenderFieldHandler implements FieldHandler {
     @Override
-    public void handleInputField(Contact contact, String field) throws FieldHandlerException {
+    public void handleInputField(Contact contact, String field) {
         if (Validator.isGender(field)) {
             contact.setGender(field);
         } else {
-            throw new FieldHandlerException("Invalid gender.");
+            contact.setGender(null);
         }
     }
 }
